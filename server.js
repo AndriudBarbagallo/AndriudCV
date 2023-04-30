1cd28ef5be81213edc00ae7889cfda61ebd0c2af
AndriudBarbagallo <andriudbarbagallo@gmail.com>
2023-04-30 18:59:27 +0100

    Undo server change.

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const app = require('./app')
 
 const port = process.env.PORT || 3000
 
-app.post('/location_specific_service', async (req, res) => {
-  var fetch_res = await fetch(`https://ipapi.co/${req.ip}/json/`);
-  var fetch_data = await res.json()
-
-  res.send(`You are from ${fetch_data.region}`)
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
